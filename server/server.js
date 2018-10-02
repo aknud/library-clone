a5b1773b1b53dd10970e5fb99cd7b59891d266ca
Amy Knudson <amylknudson@gmail.com>
2018-10-02 14:58:58 -0600

    Everything set up from old project

---
+++ server/server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const massive = require('massive');
+const bodyParser = require('body-parser');
+const ctrl = require('./controllers');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET,} = process.env;
+
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
+
+massive(CONNECTION_STRING).then(db =>{
+    app.set('db', db);
+    console.log('Database reporting for duty.');
+})
+
+app.post('/api/auth/login', ctrl.login);
+app.post('/api/auth/register', ctrl.register);
+app.post('/api/auth/logout', ctrl.logout);
+
+app.listen(SERVER_PORT, () => console.log(`Listening on ${SERVER_PORT}`))
---
+++ src/components/Auth.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {Jumbotron} from 'reactstrap';
+import axios from 'axios';
+import { getUserData } from './../ducks/reducer';
+import book from './../assets/maroon-logo.svg';
+
+class Auth extends Component {
+	constructor() {
+		super();
+		this.state = {
+			username: '',
+			password: ''
+		};
+	}
+	handleInput = (e) => {
+		this.setState({ [e.target.name]: e.target.value });
+	};
+	handleClick = () => {
+		const { username, password } = this.state;
+		axios
+			.post('/api/auth/register', { username, password })
+			.then((res) => {
+				this.props.getUserData(res.data);
+				console.log('response', res.data);
+			})
+			.catch((err) => console.log('Something wrong in frontend', err));
+	};
+	clearState = () => {
+		this.setState({ username: '', password: '' });
+	};
+	render() {
+		console.log(this.state);
+		return (
+			<div className="av-main">
+				<Jumbotron className="jumbo-div">
+					<div className="av-tan">
+						<img className="av-book" src={book} alt="book" />
+						<h1>Book Exchange</h1>
+						<div className="av-black">
+							<div className="login-username">
+								<label>Username</label>
+								<input
+									type="text"
+									name="username"
+									onChange={this.handleInput}
+									value={this.state.username}
+								/>
+							</div>
+							<div className="login-password">
+								<label>Password</label>
+								<input
+									type="password"
+									name="password"
+									onChange={this.handleInput}
+									value={this.state.password}
+								/>
+							</div>
+							<div>
+								<button className="btn" onClick={this.handleClick}>
+									Register
+								</button>
+								<button className="btn">Login</button>
+							</div>
+						</div>
+					</div>
+				</Jumbotron>
+			</div>
+		);
+	}
+}
+
+export default connect(null, { getUserData })(Auth);
